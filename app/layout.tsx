dc3c90a3ba3a647dfdcec1567460307b69b76917
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2023-09-28 16:42:13 +0100

    I beg please work

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,8 +41,8 @@ export default function RootLayout({
 			>
 				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
 				<div className="relative flex flex-col h-screen">
-						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
 							<Navbar />
+						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
 							{children}
 						</main>
 						<Footer />
