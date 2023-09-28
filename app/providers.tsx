3e4c5fe5fc7d62172c5451016ea9f674f75f5536
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2023-09-28 16:33:55 +0100

    providers updated

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
-import Navbar  from "../components/Navbar";
+import Navbar  from "@/components/Navbar";
 
 export interface ProvidersProps {
 	children: React.ReactNode;
