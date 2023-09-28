a9b14230ec57a1ca3cdf2c00e18fee5f32058746
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2023-09-28 16:35:32 +0100

    grr lets try this

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,6 @@ import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
-import Navbar  from "@/components/Navbar";
 
 export interface ProvidersProps {
 	children: React.ReactNode;
@@ -14,7 +13,6 @@ export function Providers({ children, themeProps }: ProvidersProps) {
 	return (
 		<NextUIProvider>
 			<NextThemesProvider {...themeProps}>
-				<Navbar />
 				{children}
 				</NextThemesProvider>
 		</NextUIProvider>
