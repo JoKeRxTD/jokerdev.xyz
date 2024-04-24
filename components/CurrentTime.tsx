f82887c0e42bebbc501eb17cd9badbc096f97945
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2024-04-24 01:53:09 +0100

    fixed typo in current time comp

--- components/CurrentTime.tsx
+++ components/CurrentTime.tsx
@@ -1,3 +1,4 @@
+
 import { useCallback, useEffect, useState } from "react";
 import { Code } from "@nextui-org/react";
 
@@ -24,8 +25,7 @@ const TimeStatus = () => {
         <p className="text-black/80 dark:text-[#fafafa] text-sm mb-6 mt-6 text-center">
             It&apos;s currently <span className="font-semibold text-black dark:text-white"><Code color="primary" size="sm">{time}</Code></span> for me, so I&apos;m
             probably{" "}
-            <span className="font-semibold text-black dark:text-white"><Code color="primary" size="sm">{awake ? "Awake" : "Sleeping"}</Code></span>. I&apos;ll
-            get back to you soon.
+            <span className="font-semibold text-black dark:text-white"><Code color="primary" size="sm">{awake ? "Awake" : "Sleeping"}</Code></span> I&apos;ll get back to you soon.
         </p>
         </div>
     );
