cc71107a5861ef0c290676b1824fc1955c21ce0d
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2024-04-25 08:53:34 +0100

    about me page & comp updates

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -21,7 +21,7 @@ export default function AboutPage() {
 			<h1 className="text-center p-4">
 					<Code color="primary" size="lg">About Me</Code>
 				</h1>
-			<div className="flex flex-col items-center justify-center m-6 gap-2">
+			<div className="flex flex-col items-center justify-center p-6 gap-2 rounded-xl border border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30">
 				<Image
 					src="/me.jpg"
 					alt="Coming Soon"
@@ -34,8 +34,8 @@ export default function AboutPage() {
                                 I started coding in 2015 my first project was a Discord Bot Built with Discord.JS
                                 I am a very friendly person and I love to help people out with their coding problems.
                                 I am a very active person and I love to play video games and watch movies in my spare time.
-                    <Skills/>
 				</p>
+                    <Skills/>
 
 			</div>
 		</div>
