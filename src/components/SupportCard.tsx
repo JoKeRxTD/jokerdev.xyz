35ccd68ebada272a428e6755e4db1f604a621953
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2024-05-09 15:14:00 +0100

    support modal update

--- src/components/SupportCard.tsx
+++ src/components/SupportCard.tsx
@@ -24,7 +24,7 @@ export default function SupportCard() {
                                     Oh You Clicked the Special Button, This project is free to use and open source, but if you would like to support me you can do so by clicking the button below.
                                 </p>
                                 <Button
-                                    onPress={onClose}
+                                    onClick={() => window.open("https://zap-hosting.com/joker", "_blank")}
                                     target="_blank"
                                     href="https://www.google.com"
                                     variant="flat"
@@ -33,25 +33,18 @@ export default function SupportCard() {
                                 </Button>
                             </ModalBody>
                             <ModalFooter>
-                                <Button color="primary" onClick={onClose} className="text-gray-800 dark:text-gray-100 border rounded-xl border-[#3c64949f] bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-[#2e4c70b6] dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30">
+                                <Button
+                                    isIconOnly
+                                    className="sm:w-1vh sm:items-center sm:text-center sm:justify-center p-2 text-md lg:text-lg text-blue-300 dark:text-blue-800 border rounded-xl border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200"
+                                    onClick={() => window.open("https://zap-hosting.com/joker", "_blank")}
+                                >
+                                <SiEgghead
+                                        onClick={() => window.open("https://zap-hosting.com/joker", "_blank")}
+                                    />
+                                </Button>
+                                <Button color="primary" onClick={onClose} className="border rounded-xl border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-zinc-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30">
                                     Close
                                 </Button>
-                                <Tooltip className="flex flex-col items-center text-center p-2 space-y-2 justify-center"
-                                    content={
-                                        <div className="flex flex-col items-center text-center p-2 space-y-2 justify-center border rounded-xl border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30">
-                                            <p className="text-sm font-bold text-green-600 dark:text-green-800">Zap-Hosting</p>
-                                            <p className="text-sm font-normal text-green-600 dark:text-green-800">You found an easter egg!</p>
-                                            <p className="text-sm font-normal text-green-600 dark:text-green-800">Use code&nbsp;
-                                                <Link isExternal href="https://zap-hosting.com/joker">
-                                                    <Code color="success">eqsource-10</Code>
-                                                </Link>
-                                                &nbsp;for 20% off!</p>
-                                        </div>
-                                    }>
-                                    <p className="sm:w-1vh sm:items-center sm:text-center sm:justify-center p-2 text-md lg:text-lg text-green-600 dark:text-green-800 border rounded-xl border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200">
-                                        <SiEgghead />
-                                    </p>
-                                </Tooltip>
                             </ModalFooter>
                         </>
                     )}
