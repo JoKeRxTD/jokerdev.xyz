0dba70c4174c173cbcf346cdacd07c724c76e505
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2024-05-11 04:06:18 +0100

    total visits card added

--- src/components/AnalyticsDashboard.tsx
+++ src/components/AnalyticsDashboard.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { analytics } from '@/src/utils/analytics'
-import { BarChart, Card, AreaChart, BarList } from '@tremor/react'
+import { BarChart, Card } from '@tremor/react'
 import { ArrowDownRight, ArrowRight, ArrowUpRight } from 'lucide-react'
 import ReactCountryFlag from 'react-country-flag'
 import { Divider } from "@nextui-org/react";
@@ -25,13 +25,12 @@ const Badge = ({ percentage }: { percentage: number }) => {
 
   return (
     <span
-      className={`inline-flex gap-1 items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset ${
-        isPositive
+      className={`inline-flex gap-1 items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset ${isPositive
           ? positiveClassname
           : isNeutral
-          ? neutralClassname
-          : negativeClassname
-      }`}>
+            ? neutralClassname
+            : negativeClassname
+        }`}>
       {isPositive ? <ArrowUpRight className='h-3 w-3' /> : null}
       {isNeutral ? <ArrowRight className='h-3 w-3' /> : null}
       {isNegative ? <ArrowDownRight className='h-3 w-3' /> : null}
@@ -40,15 +39,28 @@ const Badge = ({ percentage }: { percentage: number }) => {
   )
 }
 
+// get total visitors count
+
+
 const AnalyticsDashboard = ({
   avgVisitorsPerDay,
   amtVisitorsToday,
   timeseriesPageviews,
   topCountries,
 }: AnalyticsDashboardProps) => {
+
+  const totalVisitors = timeseriesPageviews.reduce((acc, curr) => {
+    return (
+      acc +
+      curr.events.reduce((acc, curr) => {
+        return acc + Object.values(curr)[0]!
+      }, 0)
+    )
+  }, 0)
+
   return (
     <div className='flex flex-col gap-6'>
-      <div className='grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6'>
+      <div className='grid w-full mx-auto grid-cols-1 sm:grid-cols-3 gap-6'>
         <Card className='w-full text-center justify-center items-center flex flex-col border rounded-xl border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30'>
           <p className='text-base text-[#222222] dark:text-[#b4b3b3] font-semibold'>
             Avg. visitors/day
@@ -70,8 +82,21 @@ const AnalyticsDashboard = ({
             {amtVisitorsToday}
           </p>
         </Card>
+        <Card className='w-full text-center justify-center items-center flex flex-col border rounded-xl border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30'>
+          <p className='flex gap-2.5 items-center text-base text-[#222222] dark:text-[#b4b3b3] font-semibold'>
+            Total Visitors
+            <Badge
+              percentage={
+                (totalVisitors / Number(avgVisitorsPerDay) - 1) * 100
+              }
+            />
+          </p>
+          <p className='text-3xl  text-[#222222] dark:text-[#fafafa] font-semibold'>
+            {totalVisitors}
+          </p>
+        </Card>
       </div>
-      <Divider/>
+      <Divider />
       <Card className='flex flex-col sm:grid grid-cols-4 gap-6 text-center justify-center items-center border rounded-xl border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30'>
         <h2 className='w-full text-[#222222] dark:text-[#b4b3b3] text-center sm:left-left font-semibold text-lg'>
           This weeks top visitors:
@@ -96,26 +121,7 @@ const AnalyticsDashboard = ({
           })}
         </div>
       </Card>
-      <Divider/>
-      {/* <Card className='w-full border rounded-xl border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/3'>      
-      {timeseriesPageviews ? (
-          <AreaChart
-            allowDecimals={false}
-              colors={['zinc-800', 'zinc-500']}
-              showAnimation
-              data={timeseriesPageviews.map((day) => ({
-                name: day.date,
-                Visitors: day.events.reduce((acc, curr) => {
-                  return acc + Object.values(curr)[0]!
-                }, 0),
-              }))}
-              categories={['Visitors']}
-              index='name'
-            />
-          ) : null}
-
-      </Card>
-      <Divider/> */}
+      <Divider />
       <Card className='w-full border rounded-xl border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30'>
         {timeseriesPageviews ? (
           <BarChart
@@ -131,15 +137,13 @@ const AnalyticsDashboard = ({
             categories={['Visitors']}
             index='name'
           />
-          
+
         ) : null}
       </Card>
-      <Divider/>
+      <Divider />
     </div>
   )
 }
-
-export type { AnalyticsDashboardProps }
 
 export default AnalyticsDashboard
 
