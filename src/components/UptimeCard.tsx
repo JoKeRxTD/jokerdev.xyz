4cc5a46b10238957e468c2389042603419b0ce5e
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2024-05-11 01:17:37 +0100

    Revert "uptimecard hotfix url changes"
    This reverts commit 69b594d29738ef30ef23b81421bc649e03a6451d.

--- src/components/UptimeCard.tsx
+++ src/components/UptimeCard.tsx
@@ -21,7 +21,7 @@ function formatUrl(url: string) {
 }
 
 export const UptimeCard = () => {
-    const url = "jokerdev.xyz";
+    const url = "www.jokerdev.xyz";
     const { dailyStatusData, monthlyAverage, isOperational } = useStatusData({
         url,
     });
