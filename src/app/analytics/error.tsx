a2a2a5514a2870fc5aa068399cf42d1042b989c0
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2024-05-11 22:14:54 +0100

    slice error.messsage after 45+ chars

--- src/app/analytics/error.tsx
+++ src/app/analytics/error.tsx
@@ -1,58 +1,93 @@
-'use client'
-import { useEffect } from 'react'
+"use client";
+import { useEffect } from "react";
 import { Code } from "@nextui-org/react";
 import { ScrollShadow } from "@nextui-org/react";
 import { motion } from "framer-motion";
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/react";
 
 export default function Error({
   error,
   reset,
 }: {
-  error: Error
-  reset: () => void
+  error: Error;
+  reset: () => void;
 }) {
   useEffect(() => {
     // Log the error to an error reporting service
-    console.error(error)
-  }, [error])
+    console.error(error);
+  }, [error]);
+
+  // if error 45 characters long, slice the error message 
+  if (error.message.length > 45) {
+    error.message = error.message.slice(0, 45) + "...";
+  }
 
   return (
     <ScrollShadow hideScrollBar className="w-full h-full">
-            <motion.div
-                    initial={{ opacity: 0, scale: 0.95 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    exit={{ opacity: 0, scale: 0.95 }}
-                    transition={{ ease: "easeOut", duration: 0.15 }}
-                    className="w-full h-full"
-                >
-          <section className="flex flex-col text-center gap-4 p-6 items-center justify-center rounded-xl border py-12 md:space-y-8 border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30">
-            <div >
-            <Code color="danger" size="lg" className='mb-4'>404 Error</Code>
-              <p className="text-2xmd text-center text-gray-800 dark:text-gray-100 mb-6">
-              Oops, Looks like you have entered a void, Lets get you back to the light.
-              </p>
-              <h2 className="text-center text-gray-800 dark:text-gray-100">
-              Click the button below to go back to the light.
-              </h2>
-              <p className="text-center text-gray-800 dark:text-gray-100 p-2">
-              If you think this is a mistake, please contact us at <a href="mailto:contact@jokerdev.xyz">contact@jokerdev.xyz</a>.
-              </p>
-              <p className="text-center text-gray-800 dark:text-gray-100">
-                {error.message}
-              </p>
-            </div>
-
-            <div className="flex gap-3">
-              <button
-                onClick={reset}
-                className="flex flex-col items-center justify-center text-center border rounded-xl p-2 space-y-2 border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30"
-                title="nextui.org homepage"
+      <motion.div
+        initial={{ opacity: 0, scale: 0.95 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.95 }}
+        transition={{ ease: "easeOut", duration: 0.15 }}
+        className="w-full h-full"
+      >
+          <Card className="sm:w-[60vh] lg:w-[65vh] sm:h-[35vh] lg:h-[30vh] text-center justify-center items-center flex flex-col border rounded-xl border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30">
+            <CardHeader className="flex flex-col text-center justify-center items-center text-2xl text-primary-300 font-bold">
+              <div className="text-center pb-2">
+                <p className="text-5xl font-extrabold text-center items-center justify-center text-blue-500 dark:text-blue-800">
+                  Error
+                </p>
+              </div>
+              <Code
+                size="lg"
+                color="danger"
+                className="text-center font-bold text-zinc-800 dark:text-pink-600"
               >
-                <span className="text-default-600">Return to the light</span>
+                500 Internal Server Error
+              </Code>
+            </CardHeader>
+            <CardBody className="flex flex-wrap items-center text-center space-y-2 pb-1 justify-between">
+              <div className="text-center pb-2">
+                <p className="text-center pb-2">
+                  <Code
+                    color="danger"
+                    size="sm"
+                    className="text-center font-bold text-zinc-800 dark:text-pink-600"
+                  >
+                    {error.message}
+                  </Code>
+                </p>
+                If you think this is a mistake, please contact me at{" "}
+                <Code
+                  size="sm"
+                  className="text-zinc-800 dark:text-blue-600 font-bold cursor-pointer"
+                  color="primary"
+                  onClick={() =>
+                    window.open("mailto:contact@jokerdev.xyz", "_blank")
+                  }
+                >
+                  contact@jokerdev.xyz
+                </Code>
+              </div>
+            </CardBody>
+            <CardFooter className="flex flex-row items-center text-center justify-center gap-2">
+              <button
+                className="text-base border rounded-xl p-2 border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30"
+                color="default"
+                onClick={reset}
+              >
+                Try Again
               </button>
-            </div>
-          </section>
-    </motion.div>
-  </ScrollShadow>
-  )
+              <button
+                className="text-base border rounded-xl p-2 border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30"
+                color="default"
+                onClick={() => window.location.replace("/")}
+              >
+                Go Home
+              </button>
+            </CardFooter>
+          </Card>
+      </motion.div>
+    </ScrollShadow>
+  );
 }
