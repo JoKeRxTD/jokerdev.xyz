a289fa6b5c633be791b0f3bf476ae2d9d6e920f7
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2024-05-12 01:21:31 +0100

    supportcard icon changed

--- src/components/SupportCard.tsx
+++ src/components/SupportCard.tsx
@@ -3,6 +3,7 @@ import { Code } from "@nextui-org/react";
 import { Link } from "@nextui-org/link";
 import { SiDiscord, SiEgghead } from "react-icons/si";
 import { Tooltip } from "@nextui-org/react";
+import {PartnerIcon} from "../components/Icons";
 
 
 export default function SupportCard() {
@@ -38,9 +39,7 @@ export default function SupportCard() {
                                     className="sm:w-1vh sm:items-center sm:text-center sm:justify-center p-2 text-md lg:text-lg text-blue-300 dark:text-blue-800 border rounded-xl border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200"
                                     onClick={() => window.open("https://zap-hosting.com/joker", "_blank")}
                                 >
-                                <SiEgghead
-                                        onClick={() => window.open("https://zap-hosting.com/joker", "_blank")}
-                                    />
+                                <PartnerIcon className="text-bold" onClick={() => window.open("https://zap-hosting.com/joker", "_blank")}/>
                                 </Button>
                                 <Button color="primary" onClick={onClose} className="border rounded-xl border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-zinc-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30">
                                     Close
