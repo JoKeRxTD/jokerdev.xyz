509828a931a2c508a086e415f0d789f36642ce2e
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2024-05-17 20:29:39 +0100

    dashboard now using shadcn tabs

--- src/app/user-profile/[[...user-profile]]/page.tsx
+++ src/app/user-profile/[[...user-profile]]/page.tsx
@@ -8,8 +8,9 @@ import { useState } from "react";
 import { UserProfile } from "@clerk/nextjs";
 import NotSignedIn from "@/src/components/NotSignedIn";
 // import JoinedOrganizationList from "@/src/components/JoinedOrganizationList";
-import { Tabs, Tab } from "@nextui-org/tabs";
+// import { Tabs, Tab } from "@nextui-org/tabs";
 import { OrganizationList, OrganizationProfile } from "@clerk/nextjs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../../../components/ui/tabs"
 
 
 export default function DashboardPage() {
@@ -37,7 +38,7 @@ const CustomPage = () => {
   return (
     <div className="flex flex-wrap items-center justify-center gap-4">
       <h1>Custom Profile Page</h1>
-      <p>This is the custom profile page</p>
+      {/* <OrganizationProfile /> */}
     </div>
     
   );
@@ -46,48 +47,34 @@ const CustomPage = () => {
   return (
     <div className="flex flex-wrap items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block text-center justify-center">
-        <h1 className="text-center p-4">
-          <p className="text-5xl font-extrabold text-center items-center justify-center text-primary-300">Dashboard</p>
+        <h1 className="text-center p-2">
+          <p className="text-3xl font-extrabold text-center items-center justify-center text-primary-300">Dashboard</p>
         </h1>
         <p className="text-lg pb-8">Welcome, {user.fullName}</p>
-        <div className="flex flex-wrap items-center justify-center gap-4">
+        <div className="flex flex-wrap items-center justify-center gap-4 ">
           <Code color="primary" size="sm" className='mb-4 sm:flex-shrink '>Here you can view your profile and the organizations you have joined.</Code>
-          <Tabs aria-label="Options" color="primary" className="max-w-[80vw] w-full h-full items-center text-center justify-center flex flex-wrap">
-            <Tab key="personal" title="Personal">
-            <Card className="flex flex-wrap items-center justify-center gap-2 rounded-xl border border-gray-300  backdrop-blur-2xl dark:border-neutral-800  lg:rounded-xl lg:border bg-transparent">
-                <CardBody>
-                  <UserProfile path="/user-profile" routing="path">
-                    <UserProfile.Page
-                      label="Custom Page"
-                      url="custom"
-                      labelIcon={<DotIcon />}
-                    >
-                      <CustomPage />
-                    </UserProfile.Page>
-                    <UserProfile.Link
-                      label="Homepage"
-                      url="/"
-                      labelIcon={<DotIcon />}
-                    />
-                    <UserProfile.Page label="account" />
-                    <UserProfile.Page label="security" />
-                  </UserProfile>
-                </CardBody>
-              </Card>
-            </Tab>
-            <Tab key="organizations" title="Organizations">
-              <Card className="flex flex-wrap items-center justify-center gap-2 rounded-xl border border-gray-300  backdrop-blur-2xl dark:border-neutral-800  lg:rounded-xl lg:border bg-transparent">
-                <CardBody>
-                  {/* <OrganizationList /> */}
-                  <OrganizationProfile />
-                </CardBody>
-              </Card>
-            </Tab>
+          <Tabs defaultValue="profile" className="">
+            <TabsList>
+              <TabsTrigger value="profile">Profile</TabsTrigger>
+              <TabsTrigger value="organization">Organization</TabsTrigger>
+            </TabsList>
+            <TabsContent value="profile" className="border rounded-xl border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30">
+              <UserProfile path="/user-profile" routing="path">
+                <UserProfile.Page
+                  label="Custom Page"
+                  url="custom"
+                  labelIcon={<DotIcon />}
+                />
+              </UserProfile>
+            </TabsContent>
+            <TabsContent value="organization" className="border rounded-xl border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30">
+              <OrganizationProfile/>
+            </TabsContent>
           </Tabs>
         </div>
         <Button
           color="default"
-          className="border rounded-xl border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30"
+          className="mt-4 border rounded-xl border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30"
           onClick={() => setShow(!show)}>
           <SignOutButton />
         </Button>
