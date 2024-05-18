cf1e6b47f7b6c5ccc377c32ea97ef6385970258b
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2024-05-18 13:52:22 +0100

    cn util path fixed

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import * as ProgressPrimitive from "@radix-ui/react-progress"
 
-import { cn } from "src/utils/cn.ts"
+import { cn } from "../../utils/cn"
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import * as TabsPrimitive from "@radix-ui/react-tabs"
 
-import { cn } from "src/utils/cn.ts"
+import { cn } from "../../utils/cn"
 
 const Tabs = TabsPrimitive.Root
 
