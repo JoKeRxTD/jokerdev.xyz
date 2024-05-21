44b6c0a8a719144aeaaa21416f7fb98bf20d7fa2
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2024-05-21 12:34:44 +0100

    update section loading motion
    motion now set inView = true on first play till screen refresh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,63 @@ import Skills from "@/src/components/Skills";
 import { motion } from "framer-motion";
 import Github from "@/src/components/Github";
 import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+import { useState } from "react";
+
+function SectionAnimation() {
+	const [skillsRef, skillsInView] = useInView({ threshold: 0 });
+	const [githubRef, githubInView] = useInView({ threshold: 0 });
+	const [inView, setInView] = useState(false);
+	
+	useEffect(() => {
+		if (skillsInView) {
+			setInView(true);
+        }
+    }, [skillsInView]);
+	
+	useEffect(() => {
+		if (githubInView) {
+			setInView(true);
+		}
+	}, [githubInView]);
+	
+
+	return (
+		<motion.div
+			initial={{ opacity: 0, scale: 0.95 }}
+			animate={{ opacity: 1, scale: 1 }}
+			exit={{ opacity: 0, scale: 0.95 }}
+			transition={{ ease: "easeIn", duration: 0.15 }}
+			className="w-full h-full"
+		>
+			<div className="text-center items-center justify-center py-1 space-y-1 md:space-y-1 lg:space-y-16 lg:gap-4">
+				<Header />
+				<Divider />
+				{/* if inView false motion section if true all section are inView */}
+				<motion.div
+					ref={skillsRef}
+					initial={{ x: 1000 }}
+					animate={inView ? { x: 0 } : { x: 1000 }}
+					transition={{ duration: 1 }}
+					className="w-full"
+				>
+					<Skills />
+				</motion.div>
+				<Divider />
+				<motion.div
+					ref={githubRef}
+					initial={{ x: -1000 }}
+					animate={inView ? { x: 0 } : { x: -1000 }}
+					transition={{ duration: 1 }}
+					className="w-full"
+				>
+					<Github />
+				</motion.div>
+				<Divider />
+			</div>
+		</motion.div>
+	);
+}
 
 const Home = () => {
 	const [skillsRef, skillsInView] = useInView({ threshold: 0 });
@@ -13,35 +70,7 @@ const Home = () => {
 
 	return (
 		<ScrollShadow hideScrollBar className="w-full h-full">
-			<motion.div
-				initial={{ opacity: 0, scale: 0.95 }}
-				animate={{ opacity: 1, scale: 1 }}
-				exit={{ opacity: 0, scale: 0.95 }}
-				transition={{ ease: "easeIn", duration: 0.15 }}
-				className="w-full h-full"
-			>
-				<div className="text-center items-center justify-center py-1 space-y-1 md:space-y-1 lg:space-y-16">
-					<Header />
-					<Divider />
-					<motion.div
-						ref={skillsRef}
-						initial={{ x: 1000 }}
-						animate={{ x: skillsInView ? 0 : 1000 }}
-						transition={{ duration: 0.7 }}
-					>
-						<Skills />
-					</motion.div>
-					<Divider />
-					<motion.div
-						ref={githubRef}
-						initial={{ x: -1000 }}
-						animate={{ x: githubInView ? 0 : -1000 }}
-						transition={{ duration: 0.7 }}
-					>
-						<Github />
-					</motion.div>
-				</div>
-			</motion.div>
+			<SectionAnimation />
 		</ScrollShadow>
 	);
 };
