b9c3a13eecf02c9a8ba483b814711dfd8f7aca52
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2024-05-29 23:56:43 +0100

    remove that

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -3,7 +3,7 @@ import { Redis } from "@upstash/redis";
 export const RedisClient = () => {
   const redis = new Redis({
     url: "https://eu1-content-mackerel-39919.upstash.io",
-    token: process.env.REDIS_KEY! || "AZvvASQgZDBiZTAzMjQtZTRkNC00MzBjLWI3Y2ItNWNhZjJkNWNjMWM5NDc3MjI1YjRhMjNjNGEzMDlmNjA4M2Q0MTViMzJiZDI=",
+    token: process.env.REDIS_KEY!,
   });
 
   return redis;
