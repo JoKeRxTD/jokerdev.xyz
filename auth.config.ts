81602893fb2b06cdba6b2b9af64169cb36ee0f42
JoKeeR <33557806+JoKeRxTD@users.noreply.github.com>
2024-06-02 04:18:05 +0100

    tailwind.config.ts font added

--- auth.config.ts
+++ auth.config.ts
@@ -1,141 +1,141 @@
-import NextAuth, { NextAuthConfig } from "next-auth"
-import { Secrets } from "next-auth"
-import type { Provider } from "next-auth/providers"
-import Discord, { DiscordProfile } from "next-auth/providers/discord"
-import type { DefaultJWT } from 'next-auth/jwt';
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "@/src/lib/db"
-import chalk from "chalk"
-import { createUser, checkUserExists, updateUserRole } from "@/src/actions/actions";
+// import NextAuth, { NextAuthConfig } from "next-auth"
+// import { Secrets } from "next-auth"
+// import type { Provider } from "next-auth/providers"
+// import Discord, { DiscordProfile } from "next-auth/providers/discord"
+// import type { DefaultJWT } from 'next-auth/jwt';
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+// import prisma from "@/src/lib/db"
+// import chalk from "chalk"
+// import { createUser, checkUserExists, updateUserRole } from "@/src/actions/actions";
 
-const scopes = ['identify', 'guilds', 'guilds.members.read', 'email', 'connections'].join(' ');
+// const scopes = ['identify', 'guilds', 'guilds.members.read', 'email', 'connections'].join(' ');
 
-const authConfig = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        Discord({
-            authorization: { params: { scope: scopes } },
-            profile(profile: DiscordProfile) {
-                return {
-                    discordId: profile.id,
-                    global_name: profile.global_name || null,
-                    name: profile.global_name || null,
-                    username: profile.username,
-                    avatar: profile.avatar || "", // Assign an empty string if avatar is null
-                    // check if img is png or gif
-                    image: `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png` || `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.gif`,
-                    email: profile.email || null,
-                    email_verified: profile.verified.toString(), // Convert boolean to string
-                    flags: profile.flags.toString(), // Convert number to string
-                    premium_type: profile.premium_type.toString(), // Convert number to string
-                    public_flags: profile.public_flags.toString(), // Convert number to string
-                    banner: `https://cdn.discordapp.com/banners/${profile.id}/${profile.banner}.png` || `https://cdn.discordapp.com/banners/${profile.id}/${profile.banner}.gif`,
-                    role: "user",
-                    createdAt: "",
-                    updatedAt: ""
-                }
-            },
-        }),
-    ],
-    callbacks: {
-        async session({ session, token }: { session: any, token: DefaultJWT }) {
-            // console.log('session ~ token:', token);
-            if (session?.user) {
-                session.user.discordId = (token.profile as DiscordProfile).id;
-                session.user.avatar = (token.profile as DiscordProfile).avatar;
-                session.user.role = (token.profile as DiscordProfile).role;
-                session.user.image = (token.profile as DiscordProfile).image;
-                session.user.email = (token.profile as DiscordProfile).email;
-                session.user.email_verified = (token.profile as DiscordProfile).email_verified;
-                session.user.flags = (token.profile as DiscordProfile).flags;
-                session.user.premium_type = (token.profile as DiscordProfile).premium_type;
-                session.user.public_flags = (token.profile as DiscordProfile).public_flags;
-                session.user.banner = (token.profile as DiscordProfile).banner;
-                session.user.createdAt = (token.profile as DiscordProfile).createdAt;
-                session.user.updatedAt = (token.profile as DiscordProfile).updatedAt;
-                session.user.global_name = (token.profile as DiscordProfile).global_name;
-                session.user.name = (token.profile as DiscordProfile).name;
-                session.user.username = (token.profile as DiscordProfile).username;
+// const authConfig = {
+//     adapter: PrismaAdapter(prisma),
+//     providers: [
+//         Discord({
+//             authorization: { params: { scope: scopes } },
+//             profile(profile: DiscordProfile) {
+//                 return {
+//                     discordId: profile.id,
+//                     global_name: profile.global_name || null,
+//                     name: profile.global_name || null,
+//                     username: profile.username,
+//                     avatar: profile.avatar || "", // Assign an empty string if avatar is null
+//                     // check if img is png or gif
+//                     image: `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png` || `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.gif`,
+//                     email: profile.email || null,
+//                     email_verified: profile.verified.toString(), // Convert boolean to string
+//                     flags: profile.flags.toString(), // Convert number to string
+//                     premium_type: profile.premium_type.toString(), // Convert number to string
+//                     public_flags: profile.public_flags.toString(), // Convert number to string
+//                     banner: `https://cdn.discordapp.com/banners/${profile.id}/${profile.banner}.png` || `https://cdn.discordapp.com/banners/${profile.id}/${profile.banner}.gif`,
+//                     role: "user",
+//                     createdAt: "",
+//                     updatedAt: ""
+//                 }
+//             },
+//         }),
+//     ],
+//     callbacks: {
+//         async session({ session, token }: { session: any, token: DefaultJWT }) {
+//             // console.log('session ~ token:', token);
+//             if (session?.user) {
+//                 session.user.discordId = (token.profile as DiscordProfile).id;
+//                 session.user.avatar = (token.profile as DiscordProfile).avatar;
+//                 session.user.role = (token.profile as DiscordProfile).role;
+//                 session.user.image = (token.profile as DiscordProfile).image;
+//                 session.user.email = (token.profile as DiscordProfile).email;
+//                 session.user.email_verified = (token.profile as DiscordProfile).email_verified;
+//                 session.user.flags = (token.profile as DiscordProfile).flags;
+//                 session.user.premium_type = (token.profile as DiscordProfile).premium_type;
+//                 session.user.public_flags = (token.profile as DiscordProfile).public_flags;
+//                 session.user.banner = (token.profile as DiscordProfile).banner;
+//                 session.user.createdAt = (token.profile as DiscordProfile).createdAt;
+//                 session.user.updatedAt = (token.profile as DiscordProfile).updatedAt;
+//                 session.user.global_name = (token.profile as DiscordProfile).global_name;
+//                 session.user.name = (token.profile as DiscordProfile).name;
+//                 session.user.username = (token.profile as DiscordProfile).username;
 
-            }
-            // Somewhat hacky implementation
-            session.profile = token.profile as DiscordProfile;
-            session.secrets = token.secrets as Secrets;
-            return session;
-        },
-        async jwt({ token, profile, account }) {
-            if (account) {
-                token.secrets = {
-                    accessToken: account.access_token,
-                    refreshToken: account.refresh_token,
-                    tokenType: account.token_type,
-                    expires_at: account.expires_at
-                };
-            }
-            if (profile) {
-                token.profile = profile;
-            }
-            // console.log('jwt ~ token:', token);
-            // console.log('jwt ~ profile:', profile);
-            // console.log('jwt ~ account:', account);
-            return token;
-        }
-    },
-    pages: {
-        signIn: "/login",
-        signOut: "/logout",
-    },
-    basePath: "/api/auth",
-    secret: process.env.SECRET,
-    debug: false,
-    logger: {
-        error(message) {
-            console.log(chalk.red(message))
-        },
-        warn(message) {
-            console.log(chalk.yellow(message))
-        },
-        debug(message) {
-            console.log(chalk.blue(message))
-        },
-    },
-    events: {
-        async signIn(message) {
-            console.log(chalk.green("sign in", message))
-            const userExists = await checkUserExists(message.user.discordId!)
-            if (!userExists) {
-                await createUser(message.user)
-            } else {
-                console.log(chalk.yellow("User already exists"))
-            }
+//             }
+//             // Somewhat hacky implementation
+//             session.profile = token.profile as DiscordProfile;
+//             session.secrets = token.secrets as Secrets;
+//             return session;
+//         },
+//         async jwt({ token, profile, account }) {
+//             if (account) {
+//                 token.secrets = {
+//                     accessToken: account.access_token,
+//                     refreshToken: account.refresh_token,
+//                     tokenType: account.token_type,
+//                     expires_at: account.expires_at
+//                 };
+//             }
+//             if (profile) {
+//                 token.profile = profile;
+//             }
+//             // console.log('jwt ~ token:', token);
+//             // console.log('jwt ~ profile:', profile);
+//             // console.log('jwt ~ account:', account);
+//             return token;
+//         }
+//     },
+//     pages: {
+//         signIn: "/login",
+//         signOut: "/logout",
+//     },
+//     basePath: "/api/auth",
+//     secret: process.env.SECRET,
+//     debug: false,
+//     logger: {
+//         error(message) {
+//             console.log(chalk.red(message))
+//         },
+//         warn(message) {
+//             console.log(chalk.yellow(message))
+//         },
+//         debug(message) {
+//             console.log(chalk.blue(message))
+//         },
+//     },
+//     events: {
+//         async signIn(message) {
+//             console.log(chalk.green("sign in", message))
+//             const userExists = await checkUserExists(message.user.discordId!)
+//             if (!userExists) {
+//                 await createUser(message.user)
+//             } else {
+//                 console.log(chalk.yellow("User already exists"))
+//             }
 
-        },
-        async signOut(message) {
-            console.log(chalk.red("sign out", message))
-        },
-        async createUser(message) {
-            console.log(chalk.green("create user", message))
-            const userExists = await checkUserExists(message.user.discordId!)
-            if (!userExists) {
-                await createUser(message.user)
-            } else {
-                console.log(chalk.yellow("User already exists"))
-            }
-        },
-        async updateUser(message) {
-            console.log(chalk.yellow("update user", message))
-            await updateUserRole(message.user.discordId!, message.user.role!)
+//         },
+//         async signOut(message) {
+//             console.log(chalk.red("sign out", message))
+//         },
+//         async createUser(message) {
+//             console.log(chalk.green("create user", message))
+//             const userExists = await checkUserExists(message.user.discordId!)
+//             if (!userExists) {
+//                 await createUser(message.user)
+//             } else {
+//                 console.log(chalk.yellow("User already exists"))
+//             }
+//         },
+//         async updateUser(message) {
+//             console.log(chalk.yellow("update user", message))
+//             await updateUserRole(message.user.discordId!, message.user.role!)
             
 
-        },
-        async linkAccount(message) {
-            console.log(chalk.blue("link account", message))
-        },
-        async session(message) {
-            console.log(chalk.cyan("session", message))
-        },
-    },
+//         },
+//         async linkAccount(message) {
+//             console.log(chalk.blue("link account", message))
+//         },
+//         async session(message) {
+//             console.log(chalk.cyan("session", message))
+//         },
+//     },
 
-} satisfies NextAuthConfig;
+// } satisfies NextAuthConfig;
 
-export default authConfig
+// export default authConfig
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss"
+const { fontFamily } = require("tailwindcss/defaultTheme")
 
 const config = {
   darkMode: ["class"],
@@ -73,6 +74,9 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
       },
     },
   },
